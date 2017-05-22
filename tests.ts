a4ce309c696305dce2b645462bc6bdf605265a76
Sam <gaoshan_274@hotmail.com>
2017-05-22 15:55:02 +0800

    added tests

--- tests.ts
+++ tests.ts
@@ -1,1 +1,13 @@
 // tests go here; this will not be compiled when this package is used as a library
+
+
+OLED.initTerminal(64, 128)
+OLED.showString("hello, world!")
+OLED.showString("counter:")
+
+let item = 0
+basic.forever(() => {
+    basic.pause(1000)
+    item += 1
+    OLED.showNumber(item)
+})
