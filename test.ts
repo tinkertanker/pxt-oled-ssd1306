67780f8b0f6f0f0154523088a3f87f7b92cee9e6
atrus159 <darius.is.ru@gmail.com>
2019-08-15 11:41:35 +0800

    Fixed Typescript memory issues

--- test.ts
+++ test.ts
@@ -1,19 +1,23 @@
-// tests go here; this will not be compiled when this package is used as a library
 
-// OLED.init(128, 64)
-// for (let i = 0; i <= 100; i += 100) {
-//     OLED.drawLoading(i)
-// }
-// OLED.clear()
-// for (let i = 0; i <= 100; i += 50) {
-//     OLED.drawLoading(i)
-// }
-// OLED.clear()
-// for (let i = 0; i <= 100; i += 10) {
-//     OLED.drawLoading(i)
-// }
-// OLED.clear()
-// for (let i = 0; i <= 100; i += 1) {
-//     OLED.drawLoading(i)
-// }
-// OLED.clear()
+OLED.init(128, 64)
+OLED.writeStringNewLine("the quick brown fox jumped over the lazy dog?")
+OLED.writeStringNewLine("THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG!")
+basic.pause(1000)
+OLED.clear()
+OLED.writeString("Your magic number is ")
+OLED.writeNum(23 * 3)
+OLED.writeStringNewLine("!")
+OLED.writeNumNewLine(1)
+basic.pause(100)
+OLED.writeNumNewLine(2)
+basic.pause(100)
+OLED.writeNumNewLine(3)
+basic.pause(1000)
+for (let i = 0; i < 100; i++) {
+    OLED.drawLoading(i)
+}
+basic.pause(1000)
+OLED.clear()
+OLED.drawRectangle(10, 10, 60, 60)
+OLED.drawLine(0, 0, 128, 64)
+OLED.drawLine(0, 64, 128, 0)
